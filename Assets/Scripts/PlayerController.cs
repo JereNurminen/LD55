88c29a0b3a03fd62011c1d9c2666e96c36899b66
Jere Nurminen <jere.nurminen@reaktor.com>
2024-04-13 13:47:02 +0300

    Fix ground detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,29 +9,25 @@ public class PlayerController : MonoBehaviour
     public float gravity = 9.8f;
     public float runSpeed = 10.0f;
 
-    public float groundRaycastDistance = 1 / 16f;
+    public float groundRaycastDistance = 1 / 8f;
     public int groundRaycastCount = 2;
     public LayerMask groundLayerMask;
 
     private bool isGrounded = false;
 
     private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidBody;
 
 
     // Start is called before the first frame update
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     /* Handle Inputs */
     void HandleInputs() {
-        if (Input.GetKey(KeyCode.W)) {
-            
-            // Move forward
-        } else if (Input.GetKey(KeyCode.S)) {
-            // Move backward
-        }
         if (Input.GetKey(KeyCode.A)) {
             Debug.Log("A key is pressed");
             velocity.x = -runSpeed;
@@ -41,25 +37,35 @@ public class PlayerController : MonoBehaviour
         } else {
             velocity.x = 0;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            velocity.y = 10;
+        }
     }
 
     void HandleGravity() {
-        velocity.y = -gravity * Time.deltaTime;
+        if (!isGrounded) {
+            velocity.y = -gravity * Time.deltaTime;
+        } else {
+            velocity.y = 0;
+        }
     }
 
     void CheckForGround() {
-        Vector2 colliderBottomLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
+        Vector2 colliderBottomLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.center.y);
+        float raylength = boxCollider.bounds.size.y / 2 + groundRaycastDistance;
         float colliderWidth = boxCollider.bounds.size.x;
 
         // This actually draws one more ray than defined in groundRaycastCount, don't care about fixing it rn
         for (int i = 0; i <= groundRaycastCount; i++) {
             Vector2 origin = new Vector2(colliderBottomLeft.x + (i * ( colliderWidth / groundRaycastCount )), colliderBottomLeft.y);
-            Debug.DrawRay(origin, Vector2.down * groundRaycastDistance, Color.red);
-            RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, groundRaycastDistance, groundLayerMask);
+            Debug.DrawRay(origin, Vector2.down * raylength, Color.red);
+            RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, raylength, groundLayerMask);
             if (hit.collider != null) {
                 isGrounded = true;
                 return;
             }
+            isGrounded = false;
         }
     }
 
@@ -71,8 +77,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckForGround();
         HandleInputs();
+    }
+
+    void FixedUpdate() {
+        CheckForGround();
         ApplyVelocity();
         HandleGravity();
     }
